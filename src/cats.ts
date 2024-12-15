4e24930fe0981f33082d2c8568cdde8e739ae853
Arti <59352535+artifishvr@users.noreply.github.com>
2024-12-15 12:46:47 -0800

    cats

--- src/cats.ts
+++ src/cats.ts
@@ -18,12 +18,12 @@ export const cats: Cat[] = [
       "https://cdn.discordapp.com/avatars/88853442730168320/fd4d2306ce31b660fbe0dfc36088d5ec.png?size=4096&width=460&height=460",
     link: "https://frikken.gay",
   },
-  // {
-  //   name: "Lines",
-  //   image:
-  //     "https://cdn.bsky.app/img/avatar/plain/did:plc:x2e2cby26igd3womcx7pt5fd/bafkreigmhwd3msnraeehjsh75rmcvjt45k5vz4ubadgvwbzlqepguq26h4@jpeg",
-  //   link: "https://bsky.app/profile/linesthecat.bsky.social",
-  // },
+  {
+    name: "Lines",
+    image:
+      "https://cdn.bsky.app/img/avatar/plain/did:plc:x2e2cby26igd3womcx7pt5fd/bafkreigmhwd3msnraeehjsh75rmcvjt45k5vz4ubadgvwbzlqepguq26h4@jpeg",
+    link: "https://bsky.app/profile/linesthecat.bsky.social",
+  },
   {
     name: "FizzyApple12",
     image:
@@ -36,37 +36,30 @@ export const cats: Cat[] = [
       "https://cdn.bsky.app/img/avatar/plain/did:plc:b4s73u5sykkce3gsxykyunyu/bafkreigagoqf5hzziemm5ldnkancf4yqs7qpt5dcijvlsbjctdlkdxmewi@jpeg",
     link: "https://bsky.app/profile/yuki-vr.bsky.social",
   },
-  // {
-  //   name: "Col The Furret",
-  //   image:
-  //     "https://cdn.discordapp.com/avatars/153964575987859456/90a1f5813fe339632977b10c77a1f7e6.png?size=4096&width=460&height=460",
-  //   link: "https://twitter.com/Colberts20",
-  // },
+  {
+    name: "Col The Furret",
+    image:
+      "https://cdn.discordapp.com/avatars/153964575987859456/90a1f5813fe339632977b10c77a1f7e6.png?size=4096&width=460&height=460",
+    link: "https://twitter.com/Colberts20",
+  },
   {
     name: "Lemmons",
     image:
       "https://pbs.twimg.com/profile_images/1808953009541689348/MV-NHYff_400x400.jpg",
     link: "https://twitter.com/Lemmons__",
   },
-  // {
-  //   name: "RinMunchkin",
-  //   image:
-  //     "https://pbs.twimg.com/profile_images/1850202309961412608/A9WCxczR_400x400.jpg",
-  //   link: "https://twitter.com/MunchkinRin",
-  // },
-  // {
   {
     name: "CosmicLazers",
     image:
       "https://cdn.bsky.app/img/avatar/plain/did:plc:ydk3dsm3m2hmctyygy6yqdx7/bafkreifvoku43vzvhxbu3xlepovsqj64mwjgla4wbzazcytwgeqdk3pbgy@jpeg",
     link: "https://bsky.app/profile/cosmicgayzers.bsky.social",
   },
-  // {
-  //   name: "Vans",
-  //   image:
-  //     "https://pbs.twimg.com/profile_images/1805742073552715776/jJbAmx6u_400x400.jpg",
-  //   link: "https://twitter.com/vansdayo/status/1523101721933021184",
-  // },
+  {
+    name: "Vans",
+    image:
+      "https://pbs.twimg.com/profile_images/1805742073552715776/jJbAmx6u_400x400.jpg",
+    link: "https://twitter.com/vansdayo/status/1523101721933021184",
+  },
   {
     name: "RoseyNekoha",
     image:
